e6654331417dfe8c8d1b1b8dfb5c035b95018c79
DeGsoft <diegoezequielguillen@gmail.com>
2024-04-17 11:01:59 -0300

    [dev] feat (email) netlify function to send email

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -20,106 +20,143 @@ export function ContactForm() {
 
     const handleSubmit = async (values) => {
         console.log(values);
-    }
-
+        // https://civeloo.com/.netlify/functions/send-email        
+        fetch('/.netlify/functions/send-email', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: values.email,
+                email: values.email,
+                details: values.message,
+            }),
+        }).then((res) => {
+            if (res.status >= 400) {
+                // swal({
+                //   title: 'Oy vey...',
+                //   text: 'Message wasn\'t sent due to internal server error :-(',
+                //   type: 'error',
+                // })
+                console.error('Message wasn\'t sent due to internal server error :-(')
+            }
+            else {
+                // swal({
+                //   title: 'Thank you for getting in touch!',
+                //   text: 'We\'ll contact you shortly :-)',
+                //   type: 'success',
+                // })
+                console.log('Message sent successfully!')
+            }
+        }).catch((err) => {
+            // swal({
+            //   title: 'Oy vey...',
+            //   text: 'Message wasn\'t sent due to internal server error :-(',
+            //   type: 'error',
+            // })
+            console.error('error: ', err)
+        });
+    };
+    
     return (<View style={styles.container}>
 
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={handleSubmit}
-                >
-                    {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-                        <View style={styles.form}>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                <View style={styles.form}>
 
-                            <View style={styles.labelContainer}>
-                                <Text style={styles.label}>
-                                    Email:
-                                </Text>
-                                {errors.email && touched.email ? <Text style={styles.error}>{errors.email}</Text> : null}
-                            </View>
-                            <TextInput
-                                style={styles.input}
-                                onChangeText={handleChange('email')}
-                                onBlur={handleBlur('email')}
-                                value={values.email}
-                            />
+                    <View style={styles.labelContainer}>
+                        <Text style={styles.label}>
+                            Email:
+                        </Text>
+                        {errors.email && touched.email ? <Text style={styles.error}>{errors.email}</Text> : null}
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        value={values.email}
+                    />
 
-                            <View style={styles.labelContainer}>
-                                <Text style={styles.label}>
-                                    Message:
-                                </Text>
-                                {errors.message && touched.message ? <Text style={styles.error}>{errors.message}</Text> : null}
-                            </View>
-                            <TextInput
-                                multiline
-                                style={styles.textArea}
-                                onChangeText={handleChange('message')}
-                                onBlur={handleBlur('message')}
-                                value={values.message}
-                            />
+                    <View style={styles.labelContainer}>
+                        <Text style={styles.label}>
+                            Message:
+                        </Text>
+                        {errors.message && touched.message ? <Text style={styles.error}>{errors.message}</Text> : null}
+                    </View>
+                    <TextInput
+                        multiline
+                        style={styles.textArea}
+                        onChangeText={handleChange('message')}
+                        onBlur={handleBlur('message')}
+                        value={values.message}
+                    />
 
-                            <Pressable
-                                style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]}
-                                onPress={handleSubmit}>
-                                <Text style={styles.button} onPress={handleSubmit}>
-                                    Submit
-                                </Text>
-                            </Pressable>
-                        </View>
-                    )}
-                </Formik>
-            </View>);
+                    <Pressable
+                        style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]}
+                        onPress={handleSubmit}>
+                        <Text style={styles.button} onPress={handleSubmit}>
+                            Submit
+                        </Text>
+                    </Pressable>
+                </View>
+            )}
+        </Formik>
+    </View>);
 }
 
-            const styles = StyleSheet.create({
-                container: {
-                top: -20,
-            display: 'grid',
-            placeItems: 'center',
-            height: '100vh',
-            maxWidth: 1024,
-            margin: 'auto',
-            color: 'white',
+const styles = StyleSheet.create({
+    container: {
+        top: -20,
+        display: 'grid',
+        placeItems: 'center',
+        height: '100vh',
+        maxWidth: 1024,
+        margin: 'auto',
+        color: 'white',
     },
-            form: {
-                width: 300,
+    form: {
+        width: 300,
     },
-            labelContainer: {
-                flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginBottom: 10,
+    labelContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
     },
-            label: {
-                fontSize: 16,
-            color: 'white',
+    label: {
+        fontSize: 16,
+        color: 'white',
     },
-            error: {
-                // textAlign: 'right',
-                fontSize: 14,
-            color: 'gray',
+    error: {
+        // textAlign: 'right',
+        fontSize: 14,
+        color: 'gray',
     },
-            input: {
-                height: 40,
-            marginBottom: 20,
-            paddingHorizontal: 10,
-            backgroundColor: 'white',
-            borderRadius: 5,
+    input: {
+        height: 40,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+        backgroundColor: 'white',
+        borderRadius: 5,
     },
-            textArea: {
-                height: 100,
-            marginBottom: 20,
-            padding: 10,
-            backgroundColor: 'white',
-            borderRadius: 5,
+    textArea: {
+        height: 100,
+        marginBottom: 20,
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 5,
     },
-            button: {
-                height: 40,
-            textAlign: 'center',
-            lineHeight: 40,
-            backgroundColor: 'gray',
-            color: 'white',
-            borderRadius: 5,
-            fontSize: 16,
+    button: {
+        height: 40,
+        textAlign: 'center',
+        lineHeight: 40,
+        backgroundColor: 'gray',
+        color: 'white',
+        borderRadius: 5,
+        fontSize: 16,
     },
 });
