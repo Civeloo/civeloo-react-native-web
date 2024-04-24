2adea112372a0a2a91137cb495149434a23dac18
DeGsoft <diegoezequielguillen@gmail.com>
2024-04-24 20:22:13 -0300

    [dev] chore (nav): change navlink to image

--- src/components/nav/nav.web.jsx
+++ src/components/nav/nav.web.jsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 import "./nav.web.css";
+import { FAVICON_PNG } from '../../../assets';
 
 const NavLink = ({ href, text }) => {
     return (
@@ -26,7 +27,20 @@ const NavBar = () => {
             $$css: true,
             _: "nav-container",
         }}>
-            <NavLink href="/" text="Home" />
+
+            <Link href={"/"} asChild>
+                <Pressable>
+                    <Image
+                        source={FAVICON_PNG}
+                        style={{
+                            width: 24,
+                            height: 24,
+                        }}
+                        resizeMode="contain"
+                    />
+                </Pressable>
+            </Link>
+
             <View style={{
                 $$css: true,
                 _: "nav-button",
